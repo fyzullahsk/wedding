69b4b44811865fccefccbb6e78e5e2fbc2452c98
fyzullahsk <fyzullahshaik@gmail.com>
2024-02-24 17:32:44 +0530

    Main Page Completed

--- marriage/src/Pages/LandingPage/LandingPage.jsx
+++ marriage/src/Pages/LandingPage/LandingPage.jsx
@@ -5,7 +5,7 @@ import ImageSwiper from "../../Components/ImageSwiper/ImageSwiper";
 export default function LandingPage() {
     return (
         <>
- <nav className="navbar">
+            <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
                         ForeverTie
@@ -30,7 +30,22 @@ export default function LandingPage() {
                     <span className='greet-message'>Your Premier E-Wedding Destination</span>
                 </span>
                 <div className="image-carousel">
-                    <ImageSwiper/>
+                    <ImageSwiper />
+                </div>
+                <div className="text-content">
+                    <div className="grid-container">
+                        <div className="grid-item">
+                            <span className="left-grid-item">WHAT WE BELIEVE</span>
+                        </div>
+                        <div className="grid-item">
+                            <div className="grid-item-content">
+                                <span >We believe in celebrating love.</span>
+                                <span>Venue.Caterer.Decor.Arrangements.Destination Wedding. Plan ahead.Seamless transaction.Hessle-free celebration.</span>
+                                <span>We believe that every love story deserves to be celebrated with grace, elegance, and authenticity, regardless of the distance or circumstances. Join us in embracing the future of weddings, where innovation meets tradition, and love knows no bounds.</span>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </>
